554eb82f5bb2e0902497310b0db11ff3bcfaf41b
Manuel Naujoks <mnaujoks@gmail.com>
2012-04-30 17:22:40 +0200

    roslyn points and buildsave project

---
+++ source/BuildSave/BuildSave/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace andrena.BuildSave
+{
+    static class GuidList
+    {
+        public const string guidBuildSavePkgString = "d54febd1-62de-45c7-8553-8bb301d8f450";
+        public const string guidBuildSaveCmdSetString = "d25fbb17-dbac-4450-b2a7-af56d4984d1a";
+        public const string guidToolWindowPersistanceString = "353f0be2-791f-440a-9609-d176005d4fb3";
+
+        public static readonly Guid guidBuildSaveCmdSet = new Guid(guidBuildSaveCmdSetString);
+    };
+}
---
+++ source/BuildSave/BuildSave/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace andrena.BuildSave
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidBuildSave =    0x101;
+
+    };
+}
